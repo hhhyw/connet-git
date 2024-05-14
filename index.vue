29030e1327d8e023c611a9586d68e9adb0f5b8f3
jiaruwonianshaoyouwei <hh660911@163.com>
2024-05-14 20:29:55 +0800

    init

---
+++ index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>测试</div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
+<style lang="" scoped></style>
